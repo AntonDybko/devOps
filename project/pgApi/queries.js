d1803612ee6309f83914466f5d2d48902caec86b
AntonDybko <dybko.anton11@ukr.net>
2024-01-10 21:43:52 +0100

    Jenkins

--- project/pgApi/queries.js
+++ project/pgApi/queries.js
@@ -1,6 +1,7 @@
 
 const gameQueries = {
     getGames: "SELECT * FROM games",
+    getGame: "SELECT g FROM games g WHERE g.id = $1",
     checkGameExists: "SELECT g FROM games g WHERE g.title = $1",
     postGame: "INSERT INTO games (title, genre, release_year) VALUES ($1, $2, $3) RETURNING *",
     updateGame: "UPDATE games SET title = $1, genre = $2, release_year = $3 WHERE id = $4 RETURNING *",
