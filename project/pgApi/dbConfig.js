1e9953443a2f2620bc74590b66eae124641a8ea3
AntonDybko <dybko.anton11@ukr.net>
2024-01-11 10:41:44 +0100

    jenkins_change

--- project/pgApi/dbConfig.js
+++ project/pgApi/dbConfig.js
@@ -2,10 +2,10 @@ const { Pool } = require("pg");
 
 const pool = new Pool({
     user: process.env.POSTGRES_USER || "postgres",
-    host: process.env.POSTGRES_HOST,
+    host: process.env.POSTGRES_HOST || "postgres-container",
     database: process.env.POSTGRES_DB || "postgres",
     password: process.env.POSTGRES_PASSWORD || "admin",
-    port: process.env.POSTGRES_PORT
+    port: process.env.POSTGRES_PORT || 8000,
 });
 
 module.exports = pool;
