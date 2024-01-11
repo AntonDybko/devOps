03787fac096e30e6276078b6f25328aa86375778
AntonDybko <dybko.anton11@ukr.net>
2024-01-11 15:17:51 +0100

    jenkins_config

--- project/pgApi/dbConfig.js
+++ project/pgApi/dbConfig.js
@@ -1,5 +1,12 @@
 const { Pool } = require("pg");
 
+/*const pool = new Pool({
+    user: process.env.POSTGRES_USER || "postgres",
+    host: process.env.POSTGRES_HOST,
+    database: process.env.POSTGRES_DB || "postgres",
+    password: process.env.POSTGRES_PASSWORD || "admin",
+    port: process.env.POSTGRES_PORT || 5432
+});*/
 const pool = new Pool({
     user: process.env.POSTGRES_USER || "postgres",
     host: process.env.POSTGRES_HOST,
--- project/pgApi/server.test.js
+++ project/pgApi/server.test.js
@@ -7,6 +7,14 @@ describe('Testy serwera', () => {
   //GET
   it('Powinien zwrócić status 200 dla GET /healthz', async () => {
     const response = await axios.get(`${apiUrl}/healthz`);
+    const config = {
+      user: process.env.POSTGRES_USER || "postgres",
+      host: process.env.POSTGRES_HOST,
+      database: process.env.POSTGRES_DB || "postgres",
+      password: process.env.POSTGRES_PASSWORD || "admin",
+      port: process.env.POSTGRES_PORT || 5432
+    }
+    console.log(config)
     expect(response.status).toBe(200);
   });
 
