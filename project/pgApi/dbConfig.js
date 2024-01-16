140e893dfe74585bbe21367afc095ed1b5cb23ce
AntonDybko <dybko.anton11@ukr.net>
2024-01-16 12:32:31 +0100

    ???

--- project/pgApi/dbConfig.js
+++ project/pgApi/dbConfig.js
@@ -1,13 +1,13 @@
 const { Pool } = require("pg");
 
-/*const pool = new Pool({
+const pool = new Pool({
     user: process.env.POSTGRES_USER || "postgres",
     host: process.env.POSTGRES_HOST || "postgres",
     database: process.env.POSTGRES_DB || "postgres",
     password: process.env.POSTGRES_PASSWORD || "admin",
-    port: process.env.POSTGRES_PORT
-});*/
-const pool = new Pool({
+    port: process.env.POSTGRES_PORT || 5432
+});
+/*const pool = new Pool({
     user: process.env.POSTGRES_USER || "postgres",
     host: process.env.POSTGRES_HOST, //|| "localhost",
     database: process.env.POSTGRES_DB || "postgres",
@@ -20,6 +20,6 @@ console.log({
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
     port: process.env.POSTGRES_PORT
-});
+});*/
 
 module.exports = pool;
