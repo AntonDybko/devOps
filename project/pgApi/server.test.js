2389456ccfc02b9170556d6402a9adecf4e3a4cb
AntonDybko <dybko.anton11@ukr.net>
2024-01-16 12:37:21 +0100

    ???

--- project/pgApi/server.test.js
+++ project/pgApi/server.test.js
@@ -1,7 +1,7 @@
 const { server } = require("./server.js");
 const axios = require("axios");
 const PORT =  process.env.PORT || 8000;
-const apiUrl = `http://localhost:${PORT}/games`;
+const apiUrl = `http://localhost:${PORT}`;
 
 describe("TEST", () => {
   it("test api work", async () => {
